f2556c0a34c1c542b626e8362fba113ae6bbe6db
Dave <ing.dave@hotmail.es>
2017-05-06 11:48:17 -0500

    Version 1.0 terminada, calculadora

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -3,14 +3,19 @@ package com.example.android.holamundo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
     private EditText cajaNumeroUno;
     private EditText cajaNumeroDos;
     private TextView cajaResultado;
-    int n1, n2, suma, resta, multi, div;
+    int n1, n2, suma;
+    private Spinner comboOpciones;
+    private String[] opciones;
+    private ArrayAdapter adapter;
 
 
     @Override
@@ -23,59 +28,46 @@ public class Principal extends AppCompatActivity {
         cajaNumeroDos=(EditText)findViewById(R.id.txtNumeroDos);
         cajaResultado=(TextView) findViewById(R.id.txtResultado);
 
-        //
+        //capturo el combo de opciones
+        comboOpciones=(Spinner)findViewById(R.id.cmbOperaciones);
+
+        //llenar los contenido de opciones
+        opciones=this.getResources().getStringArray(R.array.opciones);
+
+        //pasar el contexto
+        adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,opciones);
+
+        //forma de visualizar el adapter
+        comboOpciones.setAdapter(adapter);
 
     }
 
     //metodo para calcular
     public void calcular(View v){
-
-        double n1,n2,suma;
+        int op;
+        double n1,n2,res=0;
         if (validar()){
             n1=Double.parseDouble(cajaNumeroUno.getText().toString());
             n2=Double.parseDouble(cajaNumeroDos.getText().toString());
-            suma=n1+n2;
-            cajaResultado.setText(""+suma);
+            op=comboOpciones.getSelectedItemPosition();
+            switch (op){
+                case 0:
+                    res=n1+n2;
+                    break;
+                case 1:
+                    res=n1-n2;
+                    break;
+                case 2:
+                    res=n1*n2;
+                    break;
+                case 3:
+                    res=n1/n2;
+                    break;
+            }
+
+            cajaResultado.setText(""+res);
+            cajaNumeroUno.requestFocus();
         }
-    }
-
-    //metodo para restar
-    public void restar(View v){
-
-        double n1,n2,suma;
-        if (validar()) {
-            n1 = Double.parseDouble(cajaNumeroUno.getText().toString());
-            n2 = Double.parseDouble(cajaNumeroDos.getText().toString());
-            suma = n1 - n2;
-            cajaResultado.setText("" + suma);
-        }
-
-    }
-
-    //Metodo para multiplicar
-    public void multiplicar(View v){
-
-        double n1,n2,suma;
-        if (validar()) {
-            n1 = Double.parseDouble(cajaNumeroUno.getText().toString());
-            n2 = Double.parseDouble(cajaNumeroDos.getText().toString());
-            suma = n1 * n2;
-            cajaResultado.setText("" + suma);
-        }
-
-    }
-
-    //Metodo para dividir
-    public void dividir(View v){
-
-        double n1,n2,suma;
-        if (validar()) {
-            n1 = Double.parseDouble(cajaNumeroUno.getText().toString());
-            n2 = Double.parseDouble(cajaNumeroDos.getText().toString());
-            suma = n1 / n2;
-            cajaResultado.setText("" + suma);
-        }
-
     }
 
     //Metodo para borrar
@@ -87,7 +79,6 @@ public class Principal extends AppCompatActivity {
     }
 
 
-
     public boolean validar(){
         if (cajaNumeroUno.getText().toString().isEmpty()){
             cajaNumeroUno.requestFocus();
@@ -97,6 +88,11 @@ public class Principal extends AppCompatActivity {
         if (cajaNumeroDos.getText().toString().isEmpty()){
             cajaNumeroDos.requestFocus();
             cajaNumeroDos.setError(getResources().getString(R.string.error2));
+            return false;
+        }
+        if(comboOpciones.getSelectedItemPosition()==3 && Double.parseDouble(cajaNumeroDos.getText().toString())== 0){
+            cajaNumeroDos.requestFocus();
+            cajaNumeroDos.setError(getResources().getString(R.string.error2_division));
             return false;
         }
         return true;
